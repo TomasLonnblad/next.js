3fe44df4632a2c59df3671e6fc5dc77bff8ffdce
TomasLonnblad <tomaslonnblad@gmail.com>
2025-06-03 15:01:19 +0200

    Update next.config.js

--- examples/with-cloudinary/next.config.js
+++ examples/with-cloudinary/next.config.js
@@ -1,13 +1,20 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/my-account/**",
-        search: "",
-      },
-    ],
-  },
-};
+    unoptimized: true,
+ },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
